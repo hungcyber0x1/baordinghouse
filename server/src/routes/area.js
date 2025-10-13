bcc676efe142ff7571c62a4ebc9fe17d21fc23f3
hungcyber0x1 <hungnc0302@gmail.com>
2025-10-13 23:44:58 +0700

    Thêm tính năng đăng nhập

--- server/src/routes/area.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as controllers from '../controllers/area'
-// CRUD
-const router = express.Router()
-
-router.get('/all', controllers.getAreas)
-
-export default router
--- server/src/routes/province.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as controllers from '../controllers/province'
-// CRUD
-const router = express.Router()
-
-router.get('/all', controllers.getProvinces)
-
-export default router
